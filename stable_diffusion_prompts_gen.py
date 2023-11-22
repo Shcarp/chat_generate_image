51ee66de1a7bc7d1140a36e85ad1808e8cee1e47
hui200102 <a2191523412@gmail.com>
2023-11-22 17:59:21 +0800

    3

--- stable_diffusion_prompts_gen.py
+++ stable_diffusion_prompts_gen.py
@@ -8,25 +8,26 @@ temperature = 0.4              # A higher temperature will produce more diverse 
 top_k = 8                      # the number of tokens to sample from at each step
 max_length = 200               # the maximum number of tokens for the output of the model
 repetition_penalty = 1.2       # the penalty value for each repetition of a token
-num_return_sequences = 5       # the number of results to generate
+num_return_sequences = 1      # the number of results to generate
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-prompt = r"a dog"  # the beginning of the prompt
+while True:
+    question = input("You: ")
 
-input_ids = tokenizer(prompt, return_tensors='pt').input_ids
-output = model.generate(
-    input_ids,
-    do_sample=True,
-    temperature=temperature,
-    top_k=top_k,
-    max_length=max_length,
-    num_return_sequences=num_return_sequences,
-    repetition_penalty=repetition_penalty,
-    early_stopping=True
-)
-
-print('\033[96m' + prompt + '\033[0m')
-for i in range(len(output)):
-    print(tokenizer.decode(output[i], skip_special_tokens=True) + '\n')
+    input_ids = tokenizer(question, return_tensors='pt').input_ids
+    output = model.generate(
+        input_ids,
+        do_sample=True,
+        temperature=temperature,
+        top_k=top_k,
+        max_length=max_length,
+        num_return_sequences=num_return_sequences,
+        repetition_penalty=repetition_penalty,
+        early_stopping=True
+    )
+    print('\033[96m' + question + '\033[0m')
+    for i in range(len(output)):
+        print(tokenizer.decode(output[i], skip_special_tokens=True) + '\n')
+        
